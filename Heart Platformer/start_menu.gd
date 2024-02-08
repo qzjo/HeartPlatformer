330420b657c4319729bef57e1ca1cd2f502fbc93
@qzjo <jaocampo@s.sfusd.edu>
2024-02-08 10:55:08 -0800

    Copied files over

---
+++ Heart Platformer/start_menu.gd
@@ -0,0 +1,17 @@
+extends CenterContainer
+
+@onready var start_game_button = %StartGameButton
+
+ 
+
+func _ready():
+	RenderingServer.set_default_clear_color(Color.BLACK)
+	start_game_button.grab_focus()
+
+func _on_start_game_button_pressed():
+	await LevelTrans.fade_to_black()
+	get_tree().change_scene_to_file("res://level_one.tscn")
+	LevelTrans.fade_from_black()
+
+func _on_quit_button_pressed():
+	get_tree().quit()
---
+++ Heart Platformer/world.gd
@@ -0,0 +1,29 @@
+extends Node2D
+
+@export var next_level: PackedScene
+
+@onready var level_completed = $CanvasLayer/LevelCompleted
+@onready var start_in = %StartIn
+@onready var start_in_label = %StartInLabel
+@onready var animation_player = $AnimationPlayer
+
+func _ready():
+	Events.level_completed.connect(show_level_completed)
+	get_tree().paused = true
+	start_in.visible = true
+	await LevelTrans.fade_from_black()
+	animation_player.play("Countdown")
+	await animation_player.animation_finished
+	get_tree().paused = false
+	start_in.visible = false
+	
+func show_level_completed():
+	level_completed.show()
+	get_tree().paused = true
+	await get_tree().create_timer(1.0).timeout
+	if not next_level is PackedScene: return
+	await LevelTrans.fade_to_black()
+	get_tree().paused = false
+	get_tree().change_scene_to_packed(next_level)
+
+
